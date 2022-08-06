b96bf8d6cc3c608f73f2db32abc816af706e139a
lynnemunini <muninilynne65@gmail.com>
2022-08-06 15:23:45 +0300

    re-create an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,19 @@ class BaseModel:
         """
         Initializes a aseModel instance
         """
-        # to have a unique id for each model
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        # re-create an instance with a dictionary representation
+        if len(kwargs):
+            self.id = kwargs["id"]
+            self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.my_number = kwargs["my_number"]
+            self.name = kwargs["name"]
+        else:
+            # to have a unique id for each model
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
