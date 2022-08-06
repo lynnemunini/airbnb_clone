2afcd0a6ab9f4e3400ecf94e6be0f5d9cd61f0dd
Mark Jumba <jumbamark@yahoo.com>
2022-08-06 13:34:57 +0300

    check PEP8 style

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 """
-This module contains a BaseModel class that defines all common attributes/methods
-for other classes
+This module contains a BaseModel class that defines all common
+attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
-    class BaseModel is the parent class that defines all common attributes/methods
-    for other classes
+    class BaseModel is the parent class that defines all common
+    attributes/methods for other classes
     """
 
     def __init__(self, *args, **kwargs):
@@ -22,22 +23,24 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
         """
         Prints [<class name>] (<self.id>) <self.__dict__>
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+               format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime
+        Updates the public instance attribute updated_at with the
+        current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance
         """
         self.__dict__["__class__"] = self.__class__.__name__
         self.__dict__["created_at"] = self.created_at.isoformat()
