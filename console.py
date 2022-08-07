55a5fef583c63fa446776fbfc682471a17d69284
lynnemunini <muninilynne65@gmail.com>
2022-08-07 12:53:36 +0300

    entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Module that Contains the entry point of the command interpreter
+"""
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    contains the entry point of the command interpreter
+    """
+    intro = """
+            Welcome to the AirBnB clone CLI.
+            Type help to list commands, quit to exit.
+            """
+
+    # The prompt issued to solicit input.
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+        """
+        Method called when an empty line is 
+        entered in response to the prompt
+        """
+        pass
+    
+    def do_quit(self, args):
+        """
+        Command quit to exit the program
+        """
+        quit("See you soon!!")
+
+    def do_EOF(self, arg):
+        """
+        Command EOF to exit the program
+        """
+        sys.exit()
+
+# To make code not be executed when imported
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
